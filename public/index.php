2d06e72c601b44e1320b29e9c1611fea83c88487
rico.schulz <rico.schulz@weizmo.de>
2013-08-03 22:50:48 +0200

    remove debugging

--- public/index.php
+++ public/index.php
@@ -9,5 +9,3 @@ require_once(ROOT_DIR . 'RBMVC/Core/ClassLoader.php');
 $classLoader = new Core\ClassLoader();
 $bootstrap = new Bootstrap($classLoader);
 echo $bootstrap->run(include APPLICATION_DIR . 'data/config.php');
-
-echo '<pre>' . print_r($classLoader->getClasses(), 1) . '</pre>';
