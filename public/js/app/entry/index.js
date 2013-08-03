10178e89f281d8491938c833177d1d74bc901f54
rico.schulz <rico.schulz@weizmo.de>
2013-08-03 19:28:42 +0200

    fix bootstrap include

--- public/js/app/entry/index.js
+++ public/js/app/entry/index.js
@@ -1,4 +1,5 @@
 define(['../helper/deleteModal'
+        , '../../lib/bootstrap.min'
         , '../../lib/wysihtml5-0.3.0_rc2.min'
         , '../../lib/bootstrap-wysihtml5-0.0.2.min'
 ], function(deleteModal) {
@@ -28,7 +29,7 @@ define(['../helper/deleteModal'
             html: false, //Button which allows you to edit the generated HTML. Default false
             link: true, //Button to insert a link. Default true
             image: true, //Button to insert an image. Default true,
-            colo: true //Button to change color of font
+            colo: true //Button to change color of fonts
         });
     });
 });
