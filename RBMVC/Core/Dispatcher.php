93a8a35ddac1a276304e2ec98de5055f60966067
rico.schulz <rico.schulz@weizmo.de>
2013-08-04 15:36:24 +0200

    user dash to camel case for controller names

--- RBMVC/Core/Dispatcher.php
+++ RBMVC/Core/Dispatcher.php
@@ -1,18 +1,19 @@
 <?php
 namespace RBMVC\Core;
 
-use RBMVC\Core\View\View;
 use RBMVC\Core\ClassLoader;
 use RBMVC\Core\Controller\AbstractController;
 use RBMVC\Core\Controller\ActionHelperFactory;
+use RBMVC\Core\Utilities\Modifiers\String\DashToCamelCase;
+use RBMVC\Core\View\View;
 
 class Dispatcher {
-    
+
     /**
      * @var \RBMVC\Core\Request
      */
     private $request;
-    
+
     /**
      * @var \RBMVC\Core\View\View
      */
@@ -32,22 +33,25 @@ class Dispatcher {
      * @var void
      */
     public function setupController() {
-        $controllerName = ucfirst($this->request->getParam('controller')) . 'Controller';
-        $controller = null;
+        $controllerName  = ucfirst($this->request->getParam('controller')) . 'Controller';
+        $dashToCamelCase = new DashToCamelCase();
+        $controllerName  = $dashToCamelCase->convert($controllerName);
+
+        $controller   = null;
         $isClassError = false;
         try {
             $controller = $this->classLoader->getClassInstance($controllerName);
-        } catch(Utilities\Exception\ClassLoadingException $e) {
-            $controller = $this->classLoader->getClassInstance('IndexController');
+        } catch (Utilities\Exception\ClassLoadingException $e) {
+            $controller   = $this->classLoader->getClassInstance('IndexController');
             $isClassError = true;
         }
-        
+
         $actionHelperFactory = new ActionHelperFactory();
         $actionHelperFactory->setView($this->view);
         $actionHelperFactory->setRequest($this->request);
         $actionHelperFactory->setConfig($this->config);
         $actionHelperFactory->setClassLoader($this->classLoader);
-        
+
         if ($controller instanceof AbstractController) {
             $controller->setView($this->view);
             $controller->setRequest($this->request);
@@ -63,36 +67,40 @@ class Dispatcher {
             $controller->redirectToErrorPage(404);
         }
     }
-    
+
     /**
      * @return \RBMVC\Core\View\View
      */
     public function getView() {
         return $this->view;
     }
-    
+
     /**
      * @param \RBMVC\Core\View\View $view
+     *
      * @return \RBMVC\Core\Dispatcher
      */
     public function setView(View &$view) {
         $this->view = $view;
+
         return $this;
     }
-    
+
     /**
      * @return \RBMVC\Core\Request
      */
     public function getRequest() {
         return $this->request;
     }
-    
+
     /**
      * @param \RBMVC\Core\Request $request
+     *
      * @return \RBMVC\Core\Dispatcher
      */
     public function setRequest(Request $request) {
         $this->request = $request;
+
         return $this;
     }
 
@@ -105,20 +113,23 @@ class Dispatcher {
 
     /**
      * @param \RBMVC\Core\ClassLoader $classLoader
+     *
      * @return \RBMVC\Core\Dispatcher
      */
     public function setClassLoader(ClassLoader $classLoader) {
         $this->classLoader = $classLoader;
+
         return $this;
     }
 
-
     /**
      * @param array $config
+     *
      * @return \RBMVC\Core\Dispatcher
      */
     public function setConfig(array $config) {
         $this->config = $config;
+
         return $this;
     }
 
