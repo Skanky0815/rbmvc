d134d07bc5eb194bf7576c893e01b92883de2061
rico.schulz <rico.schulz@weizmo.de>
2013-08-04 09:00:09 +0200

    change some things for the start page

--- Application/Controller/IndexController.php
+++ Application/Controller/IndexController.php
@@ -2,10 +2,19 @@
 namespace Application\Controller;
 
 use Application\Controller\AbstractController;
+use Application\Model\Collection\EntryCollection;
 use RBMVC\Core\Utilities\SystemMessage;
 use RBMVC\Core\Utilities\Session;
 
 class IndexController extends AbstractController {
+
+    public function indexAction() {
+        $entryCollection = new EntryCollection();
+        $entryCollection->findAll();
+        $entries = $entryCollection->getModels();
+        array_splice($entries, 2);
+        $this->view->assign('entries', $entries);
+    }
     
     public function testAction() {
         $params = $this->request->getPostParams();
