75624e4dbcf63d0d7d5ebc8402d7c169e738d6e1
rico.schulz <rico.schulz@weizmo.de>
2013-08-04 14:31:31 +0200

    remove obsolete actions

--- Application/Controller/IndexController.php
+++ Application/Controller/IndexController.php
@@ -4,7 +4,6 @@ namespace Application\Controller;
 use Application\Controller\AbstractController;
 use Application\Model\Collection\EntryCollection;
 use RBMVC\Core\Utilities\SystemMessage;
-use RBMVC\Core\Utilities\Session;
 
 class IndexController extends AbstractController {
 
@@ -15,26 +14,5 @@ class IndexController extends AbstractController {
         array_splice($entries, 2);
         $this->view->assign('entries', $entries);
     }
-    
-    public function testAction() {
-        $params = $this->request->getPostParams();
-        
-        $session = new Session(('user'));
-        error_log(__METHOD__.'user ::> '.print_r($session->user, 1));
 
-        $form = new EntryForm($params);
-        if ($this->request->isPost()) {
-            $systemMessage = new SystemMessage(SystemMessage::ERROR);
-            $systemMessage->setText('invalid_form');
-            if ($form->isValid($params)) {
-                $systemMessage->setType(SystemMessage::SUCCESS);
-                $systemMessage->setTitle('save_success');
-                $systemMessage->setText('');
-            }
-            $this->addSystemMessage($systemMessage);
-        }
-        
-        $this->view->assign('form', $form);
-    }
-    
 }
