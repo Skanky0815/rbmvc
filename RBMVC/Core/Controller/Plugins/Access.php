a4721f759b94dde431e5ff207c0e467bbd80a165
rico.schulz <rico.schulz@weizmo.de>
2013-08-09 23:56:21 +0200

    implement application lib dir for business logic

---
+++ RBMVC/Core/Controller/Plugins/Access.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ricoschulz
+ * Date: 09.08.13
+ * Time: 22:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace RBMVC\Core\Controller\Plugins;
+
+use RBMVC\Core\Controller\AbstractPlugin;
+use RBMVC\Core\Request;
+
+/**
+ * Class Access
+ * @package RBMVC\Core\Controller\Plugins
+ */
+class Access extends AbstractPlugin {
+
+    /**
+     * @param Request $request
+     *
+     * @return void
+     */
+    public function onBootstrap(Request $request) {
+        $access = new \RBMVC\Core\Utilities\Access();
+
+        if (!$access->hasAccess($request->getParams())) {
+            die('you have no permission for this page');
+        }
+    }
+
+}
